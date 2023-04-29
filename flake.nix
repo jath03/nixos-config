e425b391afde2db67102343fd45ce8b67d95b604
jath03 <jackthughes@yahoo.com>
2023-04-29 12:10:27 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    sops-nix.url = github:Mic92/sops-nix;
+    devenv.url = "github:cachix/devenv/v0.5.1";
+    hyprland.url = "github:hyperwm/Hyprland";
+  };
+  
+  
+  outputs = { self, nixpkgs, sops-nix, devenv}:
+    let 
+        username = "jack";
+        # Declare the system
+        system = "x86_64-linux";
+        # Use a system-specific version of Nixpkgs
+        pkgs = import nixpkgs { inherit system; };
+    in
+    {
+        nixosConfigurations.jack-desktop = nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              hyprland.nixosModules.default
+              ./configuration.nix
+              sops-nix.nixosModules.sops
+            ];
+        };
+    };
+}
